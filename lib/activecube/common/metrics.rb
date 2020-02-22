5426b2b05d250b85e51589366a4c22dcd4d3371f
astudnev <astudnev@gmail.com>
2020-02-23 02:23:09 +0300

    metric may have pre aggregated value in commons

--- lib/activecube/common/metrics.rb
+++ lib/activecube/common/metrics.rb
@@ -12,7 +12,7 @@ module Activecube::Common
         end
       else
         define_method fname do |model, arel_table, measure, cube_query|
-          column = arel_table[self.class.column_name.to_sym]
+          column = pre_aggregate_value model, arel_table, measure, cube_query
           measure.selectors.empty? ? column.send(fname) : column.send(fname.to_s+'If', measure.condition_query(model, arel_table, cube_query))
         end
       end
@@ -20,6 +20,10 @@ module Activecube::Common
 
     end
 
+
+    def pre_aggregate_value _model, arel_table, _measure, _cube_query
+      arel_table[self.class.column_name.to_sym]
+    end
 
   end
 end

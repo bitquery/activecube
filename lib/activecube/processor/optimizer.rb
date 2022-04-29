e9dfaf8e7962a67790873587b2cb7faaabcf4674
Andrey Ivanov <ivanov17andrey@gmail.com>
2022-04-29 18:23:39 +0300

    reduced count of variants in optimizer

--- lib/activecube/processor/optimizer.rb
+++ lib/activecube/processor/optimizer.rb
@@ -6,6 +6,7 @@ module Activecube::Processor
     MAX_ITERATIONS = 3
 
     attr_reader :tables_count, :metrics_count, :cost_matrix
+
     def initialize cost_matrix
       @cost_matrix = cost_matrix
       @cache = ActiveSupport::Cache::MemoryStore.new
@@ -18,10 +19,9 @@ module Activecube::Processor
         @tables_count = cost_matrix.map(&:count).max
         @metrics_count = cost_matrix.count
 
-        (tables_count==1 || metrics_count==0) ? [0]*metrics_count : do_optimize
+        (tables_count == 1 || metrics_count == 0) ? [0] * metrics_count : do_optimize
 
-       end
-
+      end
 
     end
 
@@ -29,42 +29,96 @@ module Activecube::Processor
 
     def generate_variants vs, metric_i
 
-      return vs if metric_i==metrics_count
+      return vs if metric_i == metrics_count
 
       metric_tables = cost_matrix[metric_i].map.with_index do |c, index|
         [index] if c
       end.compact
 
-      vsnew = if metric_i==0
-         metric_tables
-      else
-        arry = []
-        vs.each do |v|
-          metric_tables.each{|newv|
-            arry << (v + newv)
-          }
-        end
-        arry
-      end
+      vsnew = if metric_i == 0
+                metric_tables
+              else
+                arry = []
+                vs.each do |v|
+                  metric_tables.each { |newv|
+                    arry << (v + newv)
+                  }
+                end
+                arry
+              end
 
-      generate_variants vsnew, metric_i+1
+      generate_variants vsnew, metric_i + 1
 
     end
 
     def cost_for variant
       variant.each_with_index.group_by(&:first).collect do |table_index, arry|
-        arry.map(&:second).map{|metric_index| cost_matrix[metric_index][table_index] }.max
+        arry.map(&:second).map { |metric_index| cost_matrix[metric_index][table_index] }.max
       end.sum
     end
 
     def do_optimize
 
-      variants = generate_variants [], 0
-      variant_costs = variants.map{|v| cost_for v}
+      # variants = generate_variants [], 0
+      variants = gen_reduced_variants
+      variant_costs = variants.map { |v| cost_for v }
       variants[variant_costs.each_with_index.min.second]
 
     end
 
-  end
+    def gen_permutations(n, k)
+      seq = *(0...n)
+      perm = seq.slice(0, k)
+      perms = []
 
+      while true
+        perms.push perm.dup
+        flag = true
+        i = 0
+        ((k - 1)...-1).step(-1).each do |ii|
+          i = ii
+          if perm[ii] < n - 1
+            flag = false
+            break
+          end
+        end
+        return perms if flag
+
+        perm[i] += 1
+        ((i + 1)...k).each { |j| perm[j] = 0 }
+      end
+    end
+
+    def gen_reduced_variants
+      # reduce size of cost_matrix deleting duplicates
+      uniq_rows = []
+      rows_indices = {}
+      cost_matrix.each_with_index do |row, i|
+        flag = false
+
+        uniq_rows.each_with_index do |u_row, j|
+          if u_row.eql? row
+            flag = true
+            rows_indices[i] = j
+          end
+        end
+
+        unless flag
+          rows_indices[i] = uniq_rows.length
+          uniq_rows.push(row)
+        end
+      end
+
+      # generating variants for reduced matrix
+      vars = gen_permutations(tables_count, uniq_rows.length)
+
+      # restore variants for full matrix
+      vars.map do |variant|
+        full_v = Array.new(metrics_count)
+        rows_indices.each { |k, v| full_v[k] = variant[v] }
+
+        full_v
+      end
+    end
+  end
 end

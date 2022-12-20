9ed53758fe3eddee00da93a413f57f2b6342ba29
Andrey Ivanov <ivanov17andrey@gmail.com>
2022-12-20 19:02:57 +0200

    any view feature

--- lib/activecube/view_definition.rb
+++ lib/activecube/view_definition.rb
@@ -1,7 +1,7 @@
 require 'active_support/concern'
 
 module Activecube::ViewDefinition
-  attr_reader :activecube_indexes, :models
+  attr_reader :activecube_indexes, :models, :any_column_tables
 
   def index(index_name, *args)
     (@activecube_indexes ||= []) << Activecube::Processor::Index.new(index_name, *args)
@@ -10,4 +10,10 @@ module Activecube::ViewDefinition
   def table(x)
     (@models ||= []) << x
   end
+
+  def any(column_name, table)
+    (@any_column_tables ||= []) << {column: column_name, table: table}
+    self.table table
+  end
+
 end
